76eec4f62a8c4df576bd2ef37ee3196fbbf670b6
Shraddha Shaw <2282577@cognizant.com>
2024-09-12 14:10:52 +0530

    Code-behind Of Download Button

--- Demo/MainWindow.xaml.cs
+++ Demo/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Net.Http;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,7 +9,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+//using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
 
 namespace Demo
 {
@@ -27,6 +30,48 @@ namespace Demo
             MenuItem menuItem = sender as MenuItem;
             MessageBox.Show($"{menuItem.Header} clicked");
         }
+        private async  void Download_ClickButtonAsync(object sender, RoutedEventArgs e)
+        {
+            string url = "https://www.learningcontainer.com/wp-content/uploads/2019/09/sample-pdf-file.pdf";
+            string filePath = @"C:\\download manager";
+            string fileExtension = Path.GetExtension(url);
+            string fileName = "downloadedFile" + fileExtension;
+            string destinationPath = Path.Combine(filePath, fileName);
+            try
+            {
+                await DownloadFileAsync(url, destinationPath);
+                MessageBox.Show("File downloaded successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+        private void DownloadButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DownloadButton.Background = new SolidColorBrush(Colors.LightGreen);
+        }
 
+        private void DownloadButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            DownloadButton.Background = new SolidColorBrush(Colors.LightGray);
+        }
+
+
+        private async Task DownloadFileAsync(string url, string filePath)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Range = new System.Net.Http.Headers.RangeHeaderValue(0, null);
+                using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    response.EnsureSuccessStatusCode();
+                    using (Stream contentStream = await response.Content.ReadAsStreamAsync(), fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+                    {
+                        await contentStream.CopyToAsync(fileStream);
+                    }
+                }
+            }
+        }
     }
 }
