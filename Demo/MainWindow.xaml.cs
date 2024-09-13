4292a4ee39b58a1e0e5b91a82d814dbc7dd84349
2270395 <2270395@LTIN489140>
2024-09-13 14:23:18 +0530

    Pushing again

--- Demo/MainWindow.xaml.cs
+++ Demo/MainWindow.xaml.cs
@@ -1,40 +1,65 @@
-﻿using System.IO;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-//using System.Windows.Shapes;
-using static System.Net.WebRequestMethods;
 
 namespace Demo
 {
-
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-
         private CancellationTokenSource _cancellationTokenSource;
         private long _downloadedBytes = 0;
-        private string _url = "https://examplefile.com/text/txt/400-mb-txt";
+        private long _totalBytes = 0;
+        private string _url = "https://github.com/szalony9szymek/large/releases/download/free/large";
         private string _filePath = @"C:\Users\2270395\Downloads";
         private string _fileName = "downloadedFile.pdf";
         private string _destinationPath;
+        private double _downloadProgress;
+        private string _downloadPercentage;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this; 
+
             _destinationPath = Path.Combine(_filePath, _fileName);
+        }
+
+        public double DownloadProgress
+        {
+            get => _downloadProgress;
+            set
+            {
+                _downloadProgress = value;
+                OnPropertyChanged(nameof(DownloadProgress));
+            }
+        }
+
+        public string DownloadPercentage
+        {
+            get => _downloadPercentage;
+            set
+            {
+                _downloadPercentage = value;
+                OnPropertyChanged(nameof(DownloadPercentage));
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private async void Download_ClickButtonAsync(object sender, RoutedEventArgs e)
         {
+            ResetDownloadProgress();
             _cancellationTokenSource = new CancellationTokenSource();
+
             try
             {
                 await DownloadFileAsync(_url, _destinationPath, _cancellationTokenSource.Token);
@@ -52,12 +77,13 @@ namespace Demo
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
         {
-            _cancellationTokenSource?.Cancel();
+            _cancellationTokenSource?.Cancel(); 
         }
 
         private async void ResumeButton_Click(object sender, RoutedEventArgs e)
         {
             _cancellationTokenSource = new CancellationTokenSource();
+
             try
             {
                 await DownloadFileAsync(_url, _destinationPath, _cancellationTokenSource.Token);
@@ -77,15 +103,29 @@ namespace Demo
         {
             using (HttpClient client = new HttpClient())
             {
-                // Resume download from where it was paused
-                client.DefaultRequestHeaders.Range = new System.Net.Http.Headers.RangeHeaderValue(_downloadedBytes, null);
+                
+                if (File.Exists(filePath))
+                {
+                    FileInfo fileInfo = new FileInfo(filePath);
+                    _downloadedBytes = fileInfo.Length; 
+                }
+
+                
+                if (_downloadedBytes > 0)
+                {
+                    client.DefaultRequestHeaders.Range = new System.Net.Http.Headers.RangeHeaderValue(_downloadedBytes, null);
+                }
+
                 using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
                 {
                     response.EnsureSuccessStatusCode();
-                    using (Stream contentStream = await response.Content.ReadAsStreamAsync(), fileStream = new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.None, 8192, true))
+                    _totalBytes = _downloadedBytes + response.Content.Headers.ContentLength.GetValueOrDefault();
+                    using (Stream contentStream = await response.Content.ReadAsStreamAsync(),
+                        fileStream = new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.None, 8192, true))
                     {
                         byte[] buffer = new byte[8192];
                         int bytesRead;
+
                         while ((bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length, cancellationToken)) > 0)
                         {
                             if (cancellationToken.IsCancellationRequested)
@@ -93,10 +133,31 @@ namespace Demo
 
                             await fileStream.WriteAsync(buffer, 0, bytesRead, cancellationToken);
                             _downloadedBytes += bytesRead;
+
+                            UpdateProgress(_downloadedBytes, _totalBytes);
                         }
                     }
                 }
             }
+        }
+
+        private void UpdateProgress(long downloadedBytes, long totalBytes)
+        {
+            double progress = (double)downloadedBytes / totalBytes * 100;
+
+            if (progress > 100)
+                progress = 100;
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                DownloadProgress = progress;
+                DownloadPercentage = $"{progress:0.00}%";
+            });
+        }
+
+        private void ResetDownloadProgress()
+        {
+            DownloadProgress = 0;
+            DownloadPercentage = "0.00%";
         }
     }
 }
