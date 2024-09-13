65b40ea28b5c8794517e56f4264eb733e063aa96
2270395 <2270395@LTIN489140>
2024-09-13 13:32:57 +0530

    Binded Progress Bar and Completion percentage

--- Demo/MainWindow.xaml.cs
+++ Demo/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Net.Http;
-using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
